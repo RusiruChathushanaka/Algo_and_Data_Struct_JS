c3671c7a14d299a08c2b72097a3c8717afc8ff5c
RusiruChathushanaka <rusiruchathushanka95@gmail.com>
2023-01-28 02:28:09 +0530

    Fibonacci Sequence
    Fibonacci Sequence function created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Algo_and_Data_Struct_JS';
+  
+  
+  ngOnInit(): void {
+    console.log(this.fibbonaci(10));
+  }
+
+  fibbonaci(n:number) {
+    const fib =[0,1]
+    for(let i=2; i<n; i++){
+      fib[i] = fib[i-1] + fib[i-2];
+    }
+    return fib;
+  }
 }
